f548e9c57312e587f20a1974b22e0590491e63ae
neocydfr <franckboudraa@gmail.com>
2018-03-14 14:20:24 +0100

    added rider show method

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -3,4 +3,9 @@ class RidersController < ApplicationController
     @riders = Rider.all
     render json: @riders
   end
+
+  def show
+    @rider = Rider.where(id: params[:id])
+    render json:@rider
+  end
 end
